94d5f9c542ae44428c756cd4171de349fe416cc0
Ivo Anjo <ivo.anjo@datadoghq.com>
2021-10-12 10:46:35 +0100

    Pre-import `Map` class, to make changing code during workshop easier

--- src/main/java/movies/Server.java
+++ src/main/java/movies/Server.java
@@ -9,6 +9,7 @@ import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Comparator;
 import java.util.List;
+import java.util.Map;
 import java.util.Random;
 import java.util.regex.Pattern;
 import java.util.stream.Collectors;
