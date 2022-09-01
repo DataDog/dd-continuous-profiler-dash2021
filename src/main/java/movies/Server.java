e37d3befaf771f81c21846b38c8e4168681ccfce
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-09-01 10:06:36 +0100

    More experiments with new endpoint

--- src/main/java/movies/Server.java
+++ src/main/java/movies/Server.java
@@ -8,6 +8,7 @@ import java.io.InputStreamReader;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.util.Comparator;
+import java.util.Collections;
 import java.util.List;
 import java.util.Map;
 import java.util.Random;
@@ -38,8 +39,8 @@ public class Server {
 	private static final Logger LOG = LoggerFactory.getLogger(Server.class);
 
 	private static final Supplier<List<Movie>> MOVIES = Suppliers.memoize(Server::loadMovies);
-	private static final Supplier<List<Credit>> CREDITS = Server::loadCredits;
-	// Placeholder for future improvement
+	private static final Supplier<List<Credit>> CREDITS = Suppliers.memoize(Server::loadCredits);
+    private static final Supplier<Map<String, List<Credit>>> CREDITS_BY_MOVIE_ID = Suppliers.memoize(() -> CREDITS.get().stream().collect(Collectors.groupingBy(c -> c.id)));
 
 	public static void main(String[] args) {
 		port(8081);
@@ -47,6 +48,7 @@ public class Server {
 		get("/credits", Server::creditsEndpoint);
 		get("/movies", Server::moviesEndpoint);
 		get("/old-movies", Server::oldMoviesEndpoint);
+		get("/stats", Server::statsEndpoint);
 
 		// Warm these up at application start
 		MOVIES.get();
@@ -66,33 +68,57 @@ public class Server {
 	private static Object creditsEndpoint(Request req, Response res) {
 		var movies = MOVIES.get().stream();
 		var query = req.queryParamOrDefault("q", req.queryParams("query"));
-		boolean stats = Boolean.parseBoolean(req.queryParams("stats"));
+		// boolean stats = Boolean.parseBoolean(req.queryParams("stats"));
 
 		if (query != null) {
-			var p = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
-			movies = movies.filter(m -> m.title != null && p.matcher(m.title).find());
+			var matchesQuery = Pattern.compile(query, Pattern.CASE_INSENSITIVE).asPredicate();
+			movies = movies.filter(m -> matchesQuery.test(m.title));
 		}
 
 		var moviesWithCredits = movies.map(movie -> new MovieWithCredits(movie, creditsForMovie(movie)));
 
-		if (stats) {
-			var moviesWithStats =
-				moviesWithCredits
-					.map(movieWithCredits -> new MovieWithCrewCount(movieWithCredits.movie, crewCountForMovie(movieWithCredits)));
-			return replyJSON(res, moviesWithStats);
-		} else {
+		// if (stats) {
+		// 	var moviesWithStats =
+		// 		moviesWithCredits
+		// 			.map(movieWithCredits -> new MovieWithCrewCount(movieWithCredits.movie, crewCountForMovie(movieWithCredits)));
+		// 	return replyJSON(res, moviesWithStats);
+		// } else {
 			return replyJSON(res, moviesWithCredits);
+		// }
+	}
+
+	private static Object statsEndpoint(Request req, Response res) {
+		var movies = MOVIES.get().stream();
+		var query = req.queryParamOrDefault("q", req.queryParams("query"));
+
+		if (query != null) {
+			var filterQuery = query.toLowerCase();
+			movies = movies.filter(m -> m.title.toLowerCase().contains(filterQuery));
 		}
+
+		var selectedMovies = movies.collect(Collectors.toList());
+
+		var numberMatched = selectedMovies.size();
+		var statsForMovies = selectedMovies.stream().map(movie -> crewCountForMovie(creditsForMovie(movie)));
+		var aggregatedStats =
+			statsForMovies
+				.flatMap(countMap -> countMap.entrySet().stream())
+				.collect(Collectors.groupingBy(Map.Entry::getKey, Collectors.summingLong(Map.Entry::getValue)));
+
+		return replyJSON(res, new StatsResult(numberMatched, aggregatedStats));
 	}
 
 	private static List<Credit> creditsForMovie(Movie movie) {
 		// Problem: We are loading the credits every time this method gets called.
 		// Problem: We are searching the entire credits list for every single movie.
-		return CREDITS.get().stream().filter(c -> c.id.equals(movie.id)).collect(Collectors.toList());
+        return CREDITS_BY_MOVIE_ID.get().get(movie.id);
 	}
 
-	private static Map<CrewRole, Long> crewCountForMovie(MovieWithCredits movie) {
-		return movie.credits.get(0).crew.stream().collect(Collectors.groupingBy(Server::parseRole, Collectors.counting()));
+	private static Map<CrewRole, Long> crewCountForMovie(List<Credit> credits) {
+		var credit = credits != null ? credits.get(0) : null;
+		return credit != null ?
+		  credit.crew.stream().collect(Collectors.groupingBy(Server::parseRole, Collectors.counting())) :
+		  Collections.emptyMap();
 	}
 
 	private static final Pattern ROLE = Pattern.compile("\\((.*)\\)");
@@ -105,6 +131,7 @@ public class Server {
 		try {
 			return CrewRole.valueOf(role);
 		} catch (IllegalArgumentException e) {
+			LOG.trace("Unknown role", e);
 			return CrewRole.Other;
 		}
 	}
@@ -201,5 +228,5 @@ public class Server {
 	public static record MovieWithCredits(Movie movie, List<Credit> credits) { }
 
 	public static enum CrewRole { Director, Writer, Screenplay, Editor, Animation, Other }
-	public static record MovieWithCrewCount(Movie movie, Map<CrewRole, Long> crewCount) { }
+	public static record StatsResult(int matchedMovies, Map<CrewRole, Long> crewCount) { }
 }
