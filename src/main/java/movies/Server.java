6b5e8e73d1565d709536bbc4955c4f9b81e6c627
Ivo Anjo <ivo.anjo@datadoghq.com>
2022-09-06 18:57:04 +0900

    A few more simplifications to example app

--- src/main/java/movies/Server.java
+++ src/main/java/movies/Server.java
@@ -22,7 +22,6 @@ import java.util.zip.GZIPInputStream;
 
 import com.google.common.base.Supplier;
 import com.google.common.base.Suppliers;
-import com.google.gson.annotations.SerializedName;
 import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.google.gson.reflect.TypeToken;
@@ -56,39 +55,26 @@ public class Server {
 		MOVIES.get();
 		CREDITS.get();
 
-		exception(Exception.class, (exception, request, response) -> {
-			System.err.println(exception.getMessage());
-			exception.printStackTrace();
-		});
+		exception(Exception.class, (exception, request, response) -> exception.printStackTrace());
 
 		LOG.info("Running version " + System.getProperty("dd.version").toLowerCase() + " with pid " + ProcessHandle.current().pid());
 	}
 
 	private static Object randomMovieEndpoint(Request req, Response res) {
-		var randomMovie = MOVIES.get().get(new Random().nextInt(MOVIES.get().size()));
-		return replyJSON(res, randomMovie);
+		return replyJSON(res, MOVIES.get().get(new Random().nextInt(MOVIES.get().size())));
 	}
 
 	private static Object creditsEndpoint(Request req, Response res) {
 		var movies = MOVIES.get().stream();
 		var query = req.queryParamOrDefault("q", req.queryParams("query"));
-		// boolean stats = Boolean.parseBoolean(req.queryParams("stats"));
 
 		if (query != null) {
-			var matchesQuery = Pattern.compile(query, Pattern.CASE_INSENSITIVE).asPredicate();
-			movies = movies.filter(m -> matchesQuery.test(m.title));
+			var p = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
+			movies = movies.filter(m -> m.title != null && p.matcher(m.title).find());
 		}
 
 		var moviesWithCredits = movies.map(movie -> new MovieWithCredits(movie, creditsForMovie(movie)));
-
-		// if (stats) {
-		// 	var moviesWithStats =
-		// 		moviesWithCredits
-		// 			.map(movieWithCredits -> new MovieWithCrewCount(movieWithCredits.movie, crewCountForMovie(movieWithCredits)));
-		// 	return replyJSON(res, moviesWithStats);
-		// } else {
-			return replyJSON(res, moviesWithCredits);
-		// }
+		return replyJSON(res, moviesWithCredits);
 	}
 
 	private static Object statsEndpoint(Request req, Response res) {
@@ -96,11 +82,11 @@ public class Server {
 		var query = req.queryParamOrDefault("q", req.queryParams("query"));
 
 		if (query != null) {
-			var filterQuery = query.toLowerCase();
-			movies = movies.filter(m -> m.lowerCaseTitle.contains(filterQuery));
+			var p = Pattern.compile(query, Pattern.CASE_INSENSITIVE);
+			movies = movies.filter(m -> m.title != null && p.matcher(m.title).find());
 		}
 
-		var selectedMovies = movies.collect(Collectors.toList());
+		var selectedMovies = movies.toList();
 
 		var numberMatched = selectedMovies.size();
 		var statsForMovies = selectedMovies.stream().map(movie -> crewCountForMovie(creditsForMovie(movie)));
@@ -113,19 +99,14 @@ public class Server {
 	}
 
 	private static List<Credit> creditsForMovie(Movie movie) {
-		// Problem: We are loading the credits every time this method gets called.
-		// Problem: We are searching the entire credits list for every single movie.
-        return CREDITS_BY_MOVIE_ID.get().get(movie.id);
+    return CREDITS_BY_MOVIE_ID.get().get(movie.id);
 	}
 
 	private static Map<CrewRole, Long> crewCountForMovie(List<Credit> credits) {
 		var credit = credits != null ? credits.get(0) : null;
 		return credit != null ?
-		  credit.crewRole.stream().collect(Collectors.groupingBy(Server::parseRole, Collectors.counting())) :
-		  Collections.emptyMap();
+			credit.crewRole.stream().collect(Collectors.groupingBy(Server::parseRole, Collectors.counting())) : Map.of();
 	}
-
-	private static final Pattern ROLE = Pattern.compile("\\((.*)\\)");
 
 	private static CrewRole parseRole(String role) {
 		try {
@@ -144,19 +125,11 @@ public class Server {
 		return role != null ? role : CrewRole.Other;
 	}
 
-	private static String getRole(String nameAndRole) {
-		var matcher = ROLE.matcher(nameAndRole);
-		matcher.find();
-		String role = matcher.group(1);
-		return role;
-	}
-
 	private static Object moviesEndpoint(Request req, Response res) {
 		var movies = MOVIES.get().stream();
 		movies = sortByDescReleaseDate(movies);
 		var query = req.queryParamOrDefault("q", req.queryParams("query"));
 		if (query != null) {
-			// Problem: We are not compiling the pattern and there's a more efficient way of ignoring cases.
 			movies = movies.filter(m -> Pattern.matches(".*" + query.toUpperCase() + ".*", m.title.toUpperCase()));
 		}
 		return replyJSON(res, movies);
@@ -164,7 +137,6 @@ public class Server {
 
 	private static Stream<Movie> sortByDescReleaseDate(Stream<Movie> movies) {
 		return movies.sorted(Comparator.comparing((Movie m) -> {
-			// Problem: We are parsing a datetime for each item to be sorted.
 			try {
 				return LocalDate.parse(m.releaseDate);
 			} catch (Exception e) {
@@ -176,10 +148,12 @@ public class Server {
 	private static Object oldMoviesEndpoint(Request req, Response res) {
 		var year = req.queryParamOrDefault("year", "2010");
 		var limit = Integer.valueOf(req.queryParamOrDefault("n", "10"));
-		var oldMovies = MOVIES.get().stream().filter(m -> isOlderThan(year, m)).collect(Collectors.toList());
+
+		var oldMovies = MOVIES.get().stream().filter(m -> isOlderThan(year, m)).toList();
 		LOG.debug("Found the following oldMovies: " + oldMovies);
-		oldMovies = oldMovies.stream().limit(limit).collect(Collectors.toList());
+		oldMovies = oldMovies.stream().limit(limit).toList();
 		LOG.debug("With limit " + limit + ", the result was: " + oldMovies);
+
 		return replyJSON(res, oldMovies);
 	}
 
@@ -189,28 +163,11 @@ public class Server {
 		return result;
 	}
 
-	private static Object replyJSON(Response res, Stream<?> data) {
-		return replyJSON(res, data.collect(Collectors.toList()));
-	}
-
+	private static Object replyJSON(Response res, Stream<?> data) { return replyJSON(res, data.toList()); }
 	private static Object replyJSON(Response res, Object data) {
 		res.type("application/json");
 		return GSON.toJson(data);
 	}
-
-  public static class FromJsonMovie {
-			String id;
-			String originalTitle;
-			String overview;
-			String releaseDate;
-			String tagline;
-			String title;
-			String voteAverage;
-
-			Movie toMovie() {
-				return new Movie(id, originalTitle, overview, releaseDate, tagline, title, voteAverage, title.toLowerCase());
-			}
-		}
 
 	private static List<Movie> loadMovies() {
 		try (
@@ -218,10 +175,10 @@ public class Server {
 			var gzis = new GZIPInputStream(is);
 			var reader = new InputStreamReader(gzis)
 		) {
-			return GSON.<List<FromJsonMovie>>fromJson(reader, new TypeToken<List<FromJsonMovie>>() {}.getType())
-				.stream().map(FromJsonMovie::toMovie).collect(Collectors.toList());
+			return GSON.<List<Movie>>fromJson(reader, new TypeToken<List<Movie>>() {}.getType())
+				.stream().map(Movie::cacheLowerCaseTitle).toList();
 		} catch (IOException e) {
-			throw new RuntimeException("Failed to load movie data");
+			throw new RuntimeException("Failed to load movie data", e);
 		}
 	}
 
@@ -230,39 +187,51 @@ public class Server {
 			var mongoClient = MongoClients.create()
 		) {
 			var creditsCollection = mongoClient.getDatabase("moviesDB").getCollection("credits");
-			return StreamSupport
-				.stream(creditsCollection.find().batchSize(5_000).map(Credit::new).spliterator(), false)
-				.collect(Collectors.toList());
+			return StreamSupport.stream(creditsCollection.find().batchSize(5_000).map(Credit::new).spliterator(), false).toList();
 		}
 	}
 
-	public static record Movie(
-		String id,
-		String originalTitle,
-		String overview,
-		String releaseDate,
-		String tagline,
-		String title,
-		String voteAverage,
-		String lowerCaseTitle // TODO: transient?
-	) {
-		public String toString() {
-			return GSON.toJson(this).toString();
+  public static class Movie {
+		String id;
+		String originalTitle;
+		String overview;
+		String releaseDate;
+		String tagline;
+		String title;
+		String voteAverage;
+		transient String lowerCaseTitle;
+
+		Movie cacheLowerCaseTitle() {
+			this.lowerCaseTitle = title.toLowerCase();
+			return this;
 		}
+
+		public String toString() { return GSON.toJson(this).toString(); }
 	}
 
-	public static record Credit(String id, List<String> crew, List<String> cast, List<String> crewRole /* TODO transient */) {
+	public static class Credit {
+		String id;
+		List<String> crew;
+		List<String> cast;
+		transient List<String> crewRole;
+
+		private static final Pattern ROLE = Pattern.compile("\\((.*)\\)");
+
 		public Credit(Document data) {
-			this(
-				data.getString("id"),
-				data.getList("crew", String.class),
-				data.getList("cast", String.class),
-				data.getList("crew", String.class).stream().map(crew -> getRole(crew)).collect(Collectors.toList())
-			);
+			this.id = data.getString("id");
+			this.crew = data.getList("crew", String.class);
+			this.cast = data.getList("cast", String.class);
+			this.crewRole = data.getList("crew", String.class).stream().map(Credit::getRole).toList();
+		}
+
+		private static String getRole(String nameAndRole) {
+			var matcher = ROLE.matcher(nameAndRole);
+			matcher.find();
+			return matcher.group(1);
 		}
 	}
-	public static record MovieWithCredits(Movie movie, List<Credit> credits) { }
+	public record MovieWithCredits(Movie movie, List<Credit> credits) { }
 
-	public static enum CrewRole { Director, Writer, Screenplay, Editor, Animation, Other }
-	public static record StatsResult(int matchedMovies, Map<CrewRole, Long> crewCount) { }
+	public enum CrewRole { Director, Writer, Screenplay, Editor, Animation, Other }
+	public record StatsResult(int matchedMovies, Map<CrewRole, Long> crewCount) { }
 }
